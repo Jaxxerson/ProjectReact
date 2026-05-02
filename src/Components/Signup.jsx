bc94a0413ad5ded12f930af64f63532d77bae205
Jaxxerson <wangairyan123@gmail.com>
2026-05-02 12:46:04 +0300

    Second Commit

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className="row justify-content-center mt-4">
+      <div className="col-md-6 card shadow p-4">
+        <h2 className='p-1'>Sign Up</h2>
+
+        <form>
+          <input type='text' placeholder='Enter Usermame' className='form-control' required/><br/>
+          <input type="text" placeholder='Enter Email' className='form-control' required/><br/>
+          <input type="password" placeholder='Enter Password' className='form-control' required /><br/>
+          <input type="text" placeholder='Enter Phone Number' className='form-control' required /><br/>
+          <input type="submit" value="Sign Up" className='btn btn-outline-danger w-100' />
+        </form>
+      </div>
+    </div>
   )
 }
 
